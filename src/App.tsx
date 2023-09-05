5d46d10f426170cb4660439aa03dc14d4994e2f5
OcroDev <rohermy.ochoa@gmail.com>
2023-09-05 01:45:08 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import SignUpInForm from './components/form';
+
+
+function App() {
+
+
+
+  return (
+    <>
+      <SignUpInForm />
+      <footer>
+        <p>
+          Created <i className="fa fa-heart"></i> by
+          <a target="_blank" href="https://github.com/OcroDev"> OcroDev</a>
+        </p>
+      </footer>
+    </>)
+}
+
+export default App
